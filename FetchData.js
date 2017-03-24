e80d828e49ec19293d489873c1d2e53484aa479c
RoyalSix <jay.a.scottjr@biola.edu>
2017-03-23 22:20:32 -0700

    added fetch data to container

--- FetchData.js
+++ FetchData.js
@@ -3,11 +3,10 @@
 const api = window.ModuleApi;
 
 
-function fetchData(params, progress, callback) {
+function fetchData(progress) {
   //Do nothing for now
   //TODO: Remove the requirement of a fetchData for sub modules
   progress(100);
-  callback();
 }
 
 
