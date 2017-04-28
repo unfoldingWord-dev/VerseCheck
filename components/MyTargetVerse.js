7c60cb9533698865ba8bfd05f5ae0f62efe75f32
EllDoubleYew <lucaswilson@mac.com>
2017-04-28 15:37:44 -0400

    Showing verse/chapter where applicable in modal now

--- components/MyTargetVerse.js
+++ components/MyTargetVerse.js
@@ -5,11 +5,18 @@ class MyTargetVerse extends Component {
 
   render() {
     let { chapter, verse, verseText, styles} = this.props
+    let chapterVerse
+
+    if(this.props.dir == "rtl"){
+      chapterVerse = verse + ":" + chapter + " "
+    }else{
+      chapterVerse = chapter + ":" + verse + " "
+    }
 
     return (
       <Col md={12} sm={12} xs={12} lg={12} style={styles}>
         <div style={{direction: this.props.dir}}>
-          <b>{chapter + ":" + verse + " "}</b>
+          <b>{chapterVerse}</b>
           {verseText}
         </div>
       </Col>
