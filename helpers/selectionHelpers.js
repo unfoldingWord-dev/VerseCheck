a92f9fb1bfc12bba208ea35fbaf028b14abc6b9a
Christopher Klapp <christopher@klapp.name>
2017-08-31 18:49:14 -0400

    All tests pass.

--- helpers/selectionHelpers.js
+++ helpers/selectionHelpers.js
@@ -7,39 +7,40 @@ import * as _ from 'lodash';
  */
 export const spliceStringOnRanges = (string, ranges) => {
   var selectionArray = [] // response
-  // sort ranges - this ensures we build the string correctly and don't miss selections
-  // concat overlaps - should not be a concern here but might help rendering bugs
   var remainingString = string
-  // shift the range since the loop is destructive
-  // by working on the remainingString and not original string
+  // shift the range since the loop is destructive by working on the remainingString and not original string
   var rangeShift = 0
-  ranges.forEach(function(rangeObject) {
-    var range = rangeObject.range
+  ranges.forEach(function(range) {
     // save all the text before the selection
-    var beforeSelection = remainingString.slice(0,range[0]-rangeShift)
-    // console.log('beforeSelection: ', beforeSelection)
+    var beforeSelection = remainingString.slice(0,range[0]-rangeShift);
     // save the text in the selection
-    var selection = remainingString.slice(range[0]-rangeShift,range[1]+1-rangeShift)
-    // console.log('subString: ', selection)
+    var selection = remainingString.slice(range[0]-rangeShift,range[1]+1-rangeShift);
     // save all the text after the selection
-    var afterSelection = remainingString.slice(range[1]-rangeShift+1)
-    // console.log('afterSelection: ', afterSelection)
-    selectionArray.push({text: beforeSelection, selected: false})
-    selectionArray.push({
-                          text: selection,
-                          selected: true,
-                          occurrence: rangeObject.occurrence,
-                          occurrences: rangeObject.occurrences
-                        })
+    var afterSelection = remainingString.slice(range[1]-rangeShift+1);
+    if (beforeSelection) {
+      selectionArray.push({text: beforeSelection, selected: false});
+    }
+    const stringBeforeRange = string.slice(0,range[0]);
+    const occurrence = occurrencesInString(stringBeforeRange, selection) + 1;
+    const occurrences = occurrencesInString(string, selection);
+    const selectionObject = {
+      text: selection,
+      selected: true,
+      occurrence: occurrence,
+      occurrences: occurrences
+    };
+    selectionArray.push(selectionObject);
     // next iteration is using remaining string
-    remainingString = afterSelection
+    remainingString = afterSelection;
     // shift the range up to last char of substring (before+sub)
-    rangeShift += beforeSelection.length
-    rangeShift += selection.length
+    rangeShift += beforeSelection.length;
+    rangeShift += selection.length;
   })
-  selectionArray.push({text: remainingString, selected: false})
+  if (remainingString) {
+    selectionArray.push({text: remainingString, selected: false});
+  }
   // remove empty text from selectionArray
-  return selectionArray
+  return selectionArray;
 }
 /**
  * @description - This converts ranges to array of selection objects
@@ -48,22 +49,18 @@ export const spliceStringOnRanges = (string, ranges) => {
  * @returns {array} - array of range objects
  */
 export const selectionsToRanges = (string, selections) => {
-  var ranges = []
-    selections.forEach(function(selection) {
+  var ranges = [];
+    selections.forEach( selection => {
       if (string !== undefined && string.includes(selection.text)) {
-        var splitArray = string.split(selection.text)
-        var beforeSelection = splitArray.slice(0,selection.occurrence).join(selection.text)
-        var start = beforeSelection.length
-        var end = start + selection.text.length - 1
-        var rangesObject = {
-                            range: [start,end],
-                            occurrence: selection.occurrence,
-                            occurrences: selection.occurrences
-                           }
-        ranges.push(rangesObject)
+        var splitArray = string.split(selection.text);
+        var beforeSelection = splitArray.slice(0,selection.occurrence).join(selection.text);
+        var start = beforeSelection.length;
+        var end = start + selection.text.length - 1;
+        var range = [start,end];
+        ranges.push(range);
       }
     })
-  return ranges
+  return ranges;
 }
 
 /**
@@ -72,7 +69,7 @@ export const selectionsToRanges = (string, selections) => {
  * @param {array} selections - array of selections [obj,...]
  * @returns {array} - array of objects
  */
-export const selectionsToSelectedStrings = (string, selections) => {
+export const selectionsToSplicedString = (string, selections) => {
   var selectedStrings = []
   var ranges = selectionsToRanges(string, selections)
   selectedStrings = spliceStringOnRanges(string, ranges)
@@ -85,28 +82,28 @@ export const selectionsToSelectedStrings = (string, selections) => {
  * @returns {array} - array of optimized ranges [[int,int],...]
  */
 export const optimizeRanges = (ranges) => {
-  var optimizedRanges = [] // response
-  ranges = _.sortBy(ranges, function(range) { return range[1] })// order ranges by end char index as secondary
-  ranges = _.sortBy(ranges, function(range) { return range[0] })// order ranges by start char index as primary
-  ranges = _.uniq(ranges) // remove duplicates
+  var optimizedRanges = []; // response
+  ranges = _.sortBy(ranges, function(range) { return range[1] }); // order ranges by end char index as secondary
+  ranges = _.sortBy(ranges, function(range) { return range[0] }); // order ranges by start char index as primary
+  ranges = _.uniq(ranges); // remove duplicates
   // combine overlapping and contiguous ranges
-  var _range = []
-  ranges.forEach(function(range, index) {
+  var _range = [];
+  ranges.forEach( (range, index) => {
     if (range[0] >= _range[0] && range[0] <= _range[1]+1) { // the start occurs in the running range and +1 handles contiguous
       if (range[1] >= _range[0] && range[1] <= _range[1]) { // if the start occurs inside running range then let's check the end
         // if the end occurs inside the running range then it's inside it and doesn't matter
       } else { // it doesn't occur inside the running range
-        _range[1] = range[1] // extend running range
+        _range[1] = range[1]; // extend running range
       }
     } else { // the start does not occur in the running range
       if (_range.length != 0) optimizedRanges.push(_range) // the running range is closed push it to optimizedRanges
-      _range = range // reset the running range to this one
+      _range = range; // reset the running range to this one
     }
     if (ranges.length === index + 1 && _range[1] - _range[0] >= 0) { // this is the last one and it isn't blank
-      optimizedRanges.push(_range) // push the last one to optimizedRanges
+      optimizedRanges.push(_range); // push the last one to optimizedRanges
     }
-  })
-  return optimizedRanges
+  });
+  return optimizedRanges;
 }
 
 /**
@@ -116,24 +113,24 @@ export const optimizeRanges = (ranges) => {
  * @returns {array} - array of selection objects
  */
  export const rangesToSelections = (string, ranges) => {
-   let selections = []
+   let selections = [];
    ranges.forEach(function(range, index) {
-     let start = range[0], end = range[1]
-     let length = end - start + 1
-     let text = string.substr(start, length) // get text
-     let regex = eval('/' + text + '/g')
-     let beforeText = string.substr(0,start)
-     let beforeMatches = beforeText.match(regex)
-     let occurrence = (beforeMatches !== null ? beforeMatches.length : 0) + 1 // get number of this occurrence
-     let occurrences = string.match(regex).length // get occurrences in string
+     let start = range[0], end = range[1];
+     let length = end - start + 1;
+     let text = string.substr(start, length); // get text
+     let regex = eval('/' + text + '/g');
+     let beforeText = string.substr(0,start);
+     let beforeMatches = beforeText.match(regex);
+     let occurrence = (beforeMatches !== null ? beforeMatches.length : 0) + 1; // get number of this occurrence
+     let occurrences = string.match(regex).length; // get occurrences in string
      let selection = {
        text: text,
        occurrence: occurrence,
        occurrences: occurrences
      };
-     selections.push(selection)
+     selections.push(selection);
    })
-   return selections
+   return selections;
  }
 
 /**
@@ -143,11 +140,11 @@ export const optimizeRanges = (ranges) => {
  * @returns {array} - array of selection objects
  */
 export const optimizeSelections = (string, selections) => {
-  let optimizedSelections // return
-  var ranges = selectionsToRanges(string, selections).map( rangeObject => rangeObject.range ) // get char ranges of each selection
-  ranges = optimizeRanges(ranges) // optimize the ranges
-  optimizedSelections = rangesToSelections(string, ranges) // convert optimized ranges into selections
-  return optimizedSelections
+  let optimizedSelections; // return
+  var ranges = selectionsToRanges(string, selections); // get char ranges of each selection
+  ranges = optimizeRanges(ranges); // optimize the ranges
+  optimizedSelections = rangesToSelections(string, ranges); // convert optimized ranges into selections
+  return optimizedSelections;
 }
 
 /**
--- test/selectionHelpers.js
+++ test/selectionHelpers.js
@@ -6,12 +6,44 @@ import isEqual from 'lodash/isEqual'
 import * as selectionHelpers from '../helpers/selectionHelpers';
 
 describe('selectionHelpers.spliceStringOnRanges', () => {
+  const string = "01 234 56789qwertyuiopasdfghjklzxcvbnmtyui01 234 567890";
   it('should splice string on ranges', function (done) {
-    const string = "01 234 56789qwertyuiopasdfghjklzxcvbnmtyui01 234 567890";
-    const ranges = [ { range: [ 45, 47 ], occurrence: 2, occurrences: 2 } ];
+    const ranges = [[45,47]];
     const output = selectionHelpers.spliceStringOnRanges(string, ranges);
     const expected = [
       { text: '01 234 56789qwertyuiopasdfghjklzxcvbnmtyui01 ', selected: false },
+      { text: '234', selected: true, occurrence: 2, occurrences: 2 },
+      { text: ' 567890', selected: false }
+    ];
+    expect(isEqual(expected, output)).to.equal(true);
+    done();
+  });
+  it('should splice string on ranges even if only first character', function (done) {
+    const ranges = [[0,0]];
+    const output = selectionHelpers.spliceStringOnRanges(string, ranges);
+    const expected = [
+      { text: '0', selected: true, occurrence: 1, occurrences: 3 },
+      { text: '1 234 56789qwertyuiopasdfghjklzxcvbnmtyui01 234 567890', selected: false }
+    ];
+    expect(isEqual(expected, output)).to.equal(true);
+    done();
+  });
+  it('should splice string on ranges even if only last character', function (done) {
+    const ranges = [[54,54]];
+    const output = selectionHelpers.spliceStringOnRanges(string, ranges);
+    const expected = [
+      { text: '01 234 56789qwertyuiopasdfghjklzxcvbnmtyui01 234 56789', selected: false },
+      { text: '0', selected: true, occurrence: 3, occurrences: 3 }
+    ];
+    expect(isEqual(expected, output)).to.equal(true);
+    done();
+  });
+  it('should splice string on ranges for two ranges', function (done) {
+    const ranges = [[0,0],[45,47]];
+    const output = selectionHelpers.spliceStringOnRanges(string, ranges);
+    const expected = [
+      { text: '0', selected: true, occurrence: 1, occurrences: 3 },
+      { text: '1 234 56789qwertyuiopasdfghjklzxcvbnmtyui01 ', selected: false },
       { text: '234', selected: true, occurrence: 2, occurrences: 2 },
       { text: ' 567890', selected: false }
     ];
@@ -33,27 +65,27 @@ describe('selectionHelpers.selectionsToRanges', () => {
   });
   it('should return ranges from selections with two selections', function (done) {
     const selections = [
-      { text: 'querty', occurrence: 1, occurrences: 1 },
+      { text: 'qwerty', occurrence: 1, occurrences: 1 },
       { text: '234', occurrence: 2, occurrences: 2 }
     ];
     const output = selectionHelpers.selectionsToRanges(string, selections);
-    const expected = [[45,47],[12,17]];
+    const expected = [[12,17],[45,47]];
     expect(isEqual(expected, output)).to.equal(true);
     done();
   });
 });
 
-describe('selectionHelpers.selectionsToSelectedStrings', () => {
+describe('selectionHelpers.selectionsToSplicedString', () => {
   const string = '01 234 56789qwertyuiopasd234fghjklzxcvbnmtyui01 234 567890';
   it('should return array of objects of selected strings from selections without spaces around it', function (done) {
     const selections = [
       { text: '234', occurrence: 2, occurrences: 3 }
     ];
-    const output = selectionHelpers.selectionsToSelectedStrings(string, selections);
+    const output = selectionHelpers.selectionsToSplicedString(string, selections);
     const expected = [
-      { text: '01 234 56789qwertyuiopasd234fghjklzxcvbnmtyui01 ', selected: false },
-      { text: '234', selected: true },
-      { text: ' 567890', selected: false }
+      { text: '01 234 56789qwertyuiopasd', selected: false },
+      { text: '234', selected: true, occurrence: 2, occurrences: 3 },
+      { text: 'fghjklzxcvbnmtyui01 234 567890', selected: false }
     ];
     expect(isEqual(expected, output)).to.equal(true);
     done();
@@ -62,11 +94,31 @@ describe('selectionHelpers.selectionsToSelectedStrings', () => {
     const selections = [
       { text: '234', occurrence: 1, occurrences: 3 }
     ];
-    const output = selectionHelpers.selectionsToSelectedStrings(string, selections);
+    const output = selectionHelpers.selectionsToSplicedString(string, selections);
     const expected = [
       { text: '01 ', selected: false },
-      { text: '234', selected: true },
+      { text: '234', selected: true, occurrence: 1, occurrences: 3 },
       { text: ' 56789qwertyuiopasd234fghjklzxcvbnmtyui01 234 567890', selected: false }
+    ];
+    expect(isEqual(expected, output)).to.equal(true);
+    done();
+  });
+  it('should return whole string as not selected when empty array input', function (done) {
+    const selections = [];
+    const output = selectionHelpers.selectionsToSplicedString(string, selections);
+    const expected = [
+      { text: '01 234 56789qwertyuiopasd234fghjklzxcvbnmtyui01 234 567890', selected: false }
+    ];
+    expect(isEqual(expected, output)).to.equal(true);
+    done();
+  });
+  it('should return whole string as selected when whole string is in selection', function (done) {
+    const selections = [
+      { text: '01 234 56789qwertyuiopasd234fghjklzxcvbnmtyui01 234 567890', occurrence: 1, occurrences: 1 }
+    ];
+    const output = selectionHelpers.selectionsToSplicedString(string, selections);
+    const expected = [
+      { text: '01 234 56789qwertyuiopasd234fghjklzxcvbnmtyui01 234 567890', selected: true, occurrence: 1, occurrences: 1 }
     ];
     expect(isEqual(expected, output)).to.equal(true);
     done();
@@ -77,7 +129,7 @@ describe('selectionHelpers.optimizeRanges', () => {
   it('should return ranges array without duplicates and overlaps merged', function (done) {
     const ranges = [[1,1],[5,9],[3,4],[7,10],[20,40],[15,16],[14,17]];
     const output = selectionHelpers.optimizeRanges(ranges);
-    const expected = [[1,1],[3,10],[14,17],[20,40] ];
+    const expected = [[1,1],[3,10],[14,17],[20,40]];
     expect(isEqual(expected, output)).to.equal(true);
     done();
   });
@@ -96,17 +148,24 @@ describe('selectionHelpers.optimizeRanges', () => {
     done();
   });
   it('should return ranges array in order', function (done) {
-    const ranges = [[5,9],[3,4],[20,40],[15,16]];
+    const ranges = [[6,9],[3,4],[20,40],[15,16]];
     const output = selectionHelpers.optimizeRanges(ranges);
-    const expected = [[3,4],[5,9],[15,16],[20,40]];
+    const expected = [[3,4],[6,9],[15,16],[20,40]];
+    expect(isEqual(expected, output)).to.equal(true);
+    done();
+  });
+  it('should return ranges empty array for empty array input', function (done) {
+    const ranges = [];
+    const output = selectionHelpers.optimizeRanges(ranges);
+    const expected = [];
     expect(isEqual(expected, output)).to.equal(true);
     done();
   });
 });
 
 describe('selectionHelpers.rangesToSelections', () => {
+  const string = "0123456789qwertyuiopasdfghjklzxcvbnmtyui01234567890";
   it('should return array of selection objects', function (done) {
-    const string = "0123456789qwertyuiopasdfghjklzxcvbnmtyui01234567890";
     const ranges = [ [3,9], [14,17], [20,40] ];
     const expected = [ { text: '3456789', occurrence: 1, occurrences: 2 },
        { text: 'tyui', occurrence: 1, occurrences: 2 },
@@ -117,9 +176,15 @@ describe('selectionHelpers.rangesToSelections', () => {
     done();
   });
   it('should return array of one selection object with one range', function (done) {
-    const string = "0123456789qwertyuiopasdfghjklzxcvbnmtyui01234567890";
     const ranges = [ [3,9] ];
     const expected = [ { text: '3456789', occurrence: 1, occurrences: 2 } ];
+     const output = selectionHelpers.rangesToSelections(string, ranges);
+    expect(isEqual(expected, output)).to.equal(true);
+    done();
+  });
+  it('should return an empty array with an empty ranges', function (done) {
+    const ranges = [];
+    const expected = [];
      const output = selectionHelpers.rangesToSelections(string, ranges);
     expect(isEqual(expected, output)).to.equal(true);
     done();
@@ -127,7 +192,7 @@ describe('selectionHelpers.rangesToSelections', () => {
 });
 
 describe('selectionHelpers.optimizeSelections', () => {
-  const string = "0123456789qwertyuiopasdfghjklzxcvbnmtyui01234567890";
+  const string = '0123456789qwertyuiopasdfghjklzxcvbnmtyui01234567890';
   it('should return selections array even with one selection', function (done) {
     const selections = [
       { text: '234', occurrence: 2, occurrences: 2 }
@@ -196,7 +261,9 @@ describe('selectionHelpers.optimizeSelections', () => {
       { text: '0', occurrence: 3, occurrences: 3 }
     ];
     const output = selectionHelpers.optimizeSelections(string, selections);
-    const expected = [];
+    const expected = [
+      { text: '0', occurrence: 3, occurrences: 3 }
+    ];
     expect(isEqual(expected, output)).to.equal(true);
     done();
   });
@@ -205,7 +272,27 @@ describe('selectionHelpers.optimizeSelections', () => {
       { text: '0', occurrence: 1, occurrences: 3 }
     ];
     const output = selectionHelpers.optimizeSelections(string, selections);
+    const expected = [
+      { text: '0', occurrence: 1, occurrences: 3 }
+    ];
+    expect(isEqual(expected, output)).to.equal(true);
+    done();
+  });
+  it('should return empty selections array with empty array input', function (done) {
+    const selections = [];
+    const output = selectionHelpers.optimizeSelections(string, selections);
     const expected = [];
+    expect(isEqual(expected, output)).to.equal(true);
+    done();
+  });
+  it('should return empty selections array with empty array input', function (done) {
+    const selections = [
+      { text: '0123456789qwertyuiopasdfghjklzxcvbnmtyui01234567890', occurrence: 1, occurrences: 1 }
+    ];
+    const output = selectionHelpers.optimizeSelections(string, selections);
+    const expected = [
+      { text: '0123456789qwertyuiopasdfghjklzxcvbnmtyui01234567890', occurrence: 1, occurrences: 1 }
+    ];
     expect(isEqual(expected, output)).to.equal(true);
     done();
   });
