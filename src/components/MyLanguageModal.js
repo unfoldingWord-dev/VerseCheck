373c3bdc24f296de1308079033c11cb3e4094ade
RoyalSix <jay.a.scottjr@biola.edu>
2018-01-11 03:20:35 -0800

    init

--- src/components/MyLanguageModal.js
+++ src/components/MyLanguageModal.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Modal, Glyphicon } from 'react-bootstrap';
 import MyTargetVerse from './MyTargetVerse';
 import ReactDOM from 'react-dom';
