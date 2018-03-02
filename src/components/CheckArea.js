da974d91d28d723d6175f87b92bf8f676afb2796
mannycolon <colonmanuel7@gmail.com>
2018-03-02 16:19:07 -0500

    fixed prop types

--- src/components/CheckArea.js
+++ src/components/CheckArea.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // components
 import DefaultArea from './DefaultArea';
@@ -7,69 +7,71 @@ import InstructionsArea from './InstructionsArea';
 import EditVerseArea from './EditVerseArea';
 import CommentArea from './CommentArea';
 import style from '../css/Style';
-
-const CheckArea = ({
-  actions,
-  mode,
-  tags,
-  verseText,
-  verseChanged,
-  comment,
-  contextIdReducer,
-  selectionsReducer,
-  projectDetailsReducer
-}) => {
-  let modeArea;
-  switch (mode) {
-    case 'edit':
-      modeArea = (
-        <EditVerseArea
-          tags={tags}
-          verseText={verseText}
-          verseChanged={verseChanged}
-          actions={actions}
-          dir={projectDetailsReducer.manifest.target_language.direction}
-        />
-      );
-      break;
-    case 'comment':
-      modeArea = <CommentArea comment={comment} actions={actions} />;
-      break;
-    case 'select':
-      modeArea = (
-        <div style={{ WebkitUserSelect: 'none', display: "flex", flex: "1", justifyContent: "center", alignItems: "center", overflow: "auto" }}>
-          <InstructionsArea
+class CheckArea extends Component {
+  render() {
+    const {
+      actions,
+      mode,
+      tags,
+      verseText,
+      verseChanged,
+      comment,
+      contextIdReducer,
+      selectionsReducer,
+      projectDetailsReducer
+    } = this.props;
+    let modeArea;
+    switch (mode) {
+      case 'edit':
+        modeArea = (
+          <EditVerseArea
+            tags={tags}
             verseText={verseText}
-            selectionsReducer={selectionsReducer}
-            quote={contextIdReducer.contextId.quote}
-            mode={mode}
+            verseChanged={verseChanged}
+            actions={actions}
+            dir={projectDetailsReducer.manifest.target_language.direction}
           />
-        </div>);
-      break;
-    case 'default':
-    default:
-      modeArea = (
-        <div style={{ WebkitUserSelect: 'none', display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
-          <InstructionsArea dontShowTranslation={true} verseText={verseText} selectionsReducer={selectionsReducer} quote={contextIdReducer.contextId.quote} />
-        </div>
-      );
-  }
+        );
+        break;
+      case 'comment':
+        modeArea = <CommentArea comment={comment} actions={actions} />;
+        break;
+      case 'select':
+        modeArea = (
+          <div style={{ WebkitUserSelect: 'none', display: "flex", flex: "1", justifyContent: "center", alignItems: "center", overflow: "auto" }}>
+            <InstructionsArea
+              verseText={verseText}
+              selectionsReducer={selectionsReducer}
+              quote={contextIdReducer.contextId.quote}
+              mode={mode}
+            />
+          </div>);
+        break;
+      case 'default':
+      default:
+        modeArea = (
+          <div style={{ WebkitUserSelect: 'none', display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
+            <InstructionsArea dontShowTranslation={true} verseText={verseText} selectionsReducer={selectionsReducer} quote={contextIdReducer.contextId.quote} />
+          </div>
+        );
+    }
 
-  return (
-    <div style={style.checkArea}>
-      {mode === 'select' ?
-        <SelectionArea
-          {...this.props}
-          actions={actions}
-          quote={contextIdReducer.contextId.quote}
-        /> :
-        <DefaultArea {...this.props} />}
-      <div style={{ borderLeft: '1px solid var(--border-color)', flex: 1, overflowY: "auto", display:'flex', justifyContent:'center' }}>
-        {modeArea}
+    return (
+      <div style={style.checkArea}>
+        {mode === 'select' ?
+          <SelectionArea
+            {...this.props}
+            actions={actions}
+            quote={contextIdReducer.contextId.quote}
+          /> :
+          <DefaultArea {...this.props} />}
+        <div style={{ borderLeft: '1px solid var(--border-color)', flex: 1, overflowY: "auto", display:'flex', justifyContent:'center' }}>
+          {modeArea}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 CheckArea.propTypes = {
   actions: PropTypes.object.isRequired,
