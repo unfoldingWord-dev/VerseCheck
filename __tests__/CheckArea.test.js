d0348d7ead753af7cf863f817124506634bd51e0
Richard Mahn <=>
2018-03-16 23:55:54 -0600

    Update to tests

--- __tests__/CheckArea.test.js
+++ __tests__/CheckArea.test.js
@@ -6,52 +6,61 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
 const ult_titus_1 = '__tests__/fixtures/ult/tit/1.json';
-const titus1 = fs.readJSONSync(ult_titus_1);
-
-const props = {
-  actions: {},
-  mode: 'default',
-  tags: [],
-  verseText: 'This is the verse text',
-  verseChanged: false,
-  comment: 'Comment!',
-  contextIdReducer: {
-    contextId: {
-      quote: 'Θεοῦ',
-      occurrence: 2,
-      occurrences: 2,
-      reference: {
-        chapter: 1,
-        verse: 1
-      }
-    }
-  },
-  selectionsReducer: {
-    selections: []
-  },
-  projectDetailsReducer: {
-    manifest: {},
-    currentProjectToolsSelectedGL: {
-      translationWords: 'en'
-    }
-  },
-  resourcesReducer: {
-    bibles: {
-      en: {
-        ult: {
-          1: {
-            1: titus1[1]
-          }
-        }
-      }
-    }
-  },
-  toolsReducer: {
-    currentToolName: 'translationWords'
-  }
-};
 
 describe('CheckArea component Tests', () => {
+  const titus1 = fs.readJSONSync(ult_titus_1);
+  let props;
+  
+  beforeEach(()=>{
+    props = {
+      actions: {},
+      mode: 'default',
+      tags: [],
+      verseText: 'This is the verse text',
+      verseChanged: false,
+      comment: 'Comment!',
+      contextIdReducer: {
+        contextId: {
+          quote: 'Θεοῦ',
+          occurrence: 2,
+          occurrences: 2,
+          reference: {
+            chapter: 1,
+            verse: 1
+          }
+        }
+      },
+      selectionsReducer: {
+        selections: []
+      },
+      selections: [],
+      projectDetailsReducer: {
+        manifest: {
+          target_language: {
+            direction: 'ltr'
+          }
+        },
+        currentProjectToolsSelectedGL: {
+          translationWords: 'en'
+        }
+      },
+      resourcesReducer: {
+        bibles: {
+          en: {
+            ult: {
+              1: {
+                1: titus1[1]
+              }
+            }
+          }
+        }
+      },
+      toolsReducer: {
+        currentToolName: 'translationWords'
+      }
+    };    
+  });
+
   test('Check CheckArea component mode default', () => {
     const wrapper = shallow(<CheckArea {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
@@ -59,31 +68,27 @@ describe('CheckArea component Tests', () => {
   });
 
   test('Test CheckArea component mode select)', () => {
-    const newProps = JSON.parse(JSON.stringify(props));
-    newProps.mode = 'select';
+    props.mode = 'select';
     const wrapper = shallow(<CheckArea {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   test('Test CheckArea component mode edit)', () => {
-    const newProps = JSON.parse(JSON.stringify(props));
-    newProps.mode = 'edit';
+    props.mode = 'edit';
     const wrapper = shallow(<CheckArea {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   test('Test CheckArea component mode comment)', () => {
-    const newProps = JSON.parse(JSON.stringify(props));
-    newProps.mode = 'comment';
+    props.mode = 'comment';
     const wrapper = shallow(<CheckArea {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 
   test('Test CheckArea component where GL Aligned Text not found (uses Greek quote instead)', () => {
-    const newProps = JSON.parse(JSON.stringify(props));
-    newProps.resourcesReducer.bibles.en.ult[1][1] = {};
-    const wrapper = shallow(<CheckArea {...newProps} />);
+    props.resourcesReducer.bibles.en.ult[1][1] = {};
+    const wrapper = shallow(<CheckArea {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
-    expect(wrapper.instance().alignedGLText).toEqual(newProps.contextIdReducer.contextId.quote);
+    expect(wrapper.instance().alignedGLText).toEqual(props.contextIdReducer.contextId.quote);
   });
 });
--- __tests__/InstructionsArea.test.js
+++ __tests__/InstructionsArea.test.js
@@ -4,17 +4,22 @@ import InstructionsArea from '../src/components/InstructionsArea';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 
-const props = {
-  alignedGLText: 'God\'s',
-  selectionsReducer: {
-    selections: [{text:'selected text'}]
-  },
-  dontShowTranslation: false,
-  verseText: 'This is the verse text',
-  mode: 'select'
-};
 
 describe('InstructionsArea component Tests', () => {
+  let props;
+
+  beforeEach(()=> {
+    props = {
+      alignedGLText: 'God\'s',
+      selectionsReducer: {
+        selections: [{text:'selected text'}]
+      },
+      dontShowTranslation: false,
+      verseText: 'This is the verse text',
+      mode: 'select'
+    };
+  });
+
   test('Check InstructionsArea component mode select', () => {
     const wrapper = shallow(<InstructionsArea {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
@@ -22,26 +27,23 @@ describe('InstructionsArea component Tests', () => {
   });
 
   test('Test InstructionsArea component default mode)', () => {
-    const newProps = JSON.parse(JSON.stringify(props));
-    newProps.mode = 'default';
-    const wrapper = shallow(<InstructionsArea {...newProps} />);
+    props.mode = 'default';
+    const wrapper = shallow(<InstructionsArea {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
-    expect(wrapper.text()).toEqual('"'+props.alignedGLText+'"has been translated as:"'+newProps.selectionsReducer.selections[0].text+'" ');
+    expect(wrapper.text()).toEqual('"'+props.alignedGLText+'"has been translated as:"'+props.selectionsReducer.selections[0].text+'" ');
   });
 
   test('Test InstructionsArea component no selections)', () => {
-    const newProps = JSON.parse(JSON.stringify(props));
-    newProps.selectionsReducer.selections = [];
-    newProps.dontShowTranslation = true;
-    const wrapper = shallow(<InstructionsArea {...newProps} />);
+    props.selectionsReducer.selections = [];
+    props.dontShowTranslation = true;
+    const wrapper = shallow(<InstructionsArea {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
     expect(wrapper.text()).toEqual('No selection has been made.Click the Select button, then select the translation for this check.');
   });
 
   test('Test InstructionsArea component no verse text)', () => {
-    const newProps = JSON.parse(JSON.stringify(props));
-    newProps.verseText = '';
-    const wrapper = shallow(<InstructionsArea {...newProps} />);
+    props.verseText = '';
+    const wrapper = shallow(<InstructionsArea {...props} />);
     expect(toJson(wrapper)).toMatchSnapshot();
     expect(wrapper.text()).toEqual('No selection can be made because the verse is blank.You may fix this by editing the verse.If desired, you may also leave a comment or bookmark this check.');
   });
