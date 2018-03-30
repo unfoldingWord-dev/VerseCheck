aa371d637e6f757a11e2145a7c40b8445f5d9ae9
bspidel <bruce.spidel@gmail.com>
2018-03-30 16:46:47 -0400

    changed last instance of goToNextOrPrevious reference to func

--- __tests__/View.test.js
+++ __tests__/View.test.js
@@ -38,7 +38,7 @@ describe('View component Tests', () => {
       cancelSelection: () => jest.fn(),
       clearSelection: () => jest.fn(),
       saveSelection: () => jest.fn(),
-      goToNextOrPrevious: () => jest.fn()
+      goToNextOrPrevious: () => null
     };
 
     const component = renderer.create(
