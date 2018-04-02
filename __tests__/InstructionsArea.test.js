d14a570ccb30f1cd11d117aaa272d829052a337a
RoyalSix <jay.a.scottjr@biola.edu>
2018-04-02 08:43:06 -0700

    added a test

--- __tests__/InstructionsArea.test.js
+++ __tests__/InstructionsArea.test.js
@@ -34,6 +34,23 @@ describe('InstructionsArea component Tests', () => {
     expect(wrapper.text()).toEqual('"'+props.alignedGLText+'"translated_as"'+props.selectionsReducer.selections[0].text+'"');
   });
 
+  test('Test InstructionsArea component default mode with 1 selection)', () => {
+    props.mode = 'default';
+    props.selectionsReducer.selections = [{text:'selected text'}, {text:'another selected text'}];
+    const wrapper = mount(<InstructionsArea {...props} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.text()).toEqual(`"${props.alignedGLText}"translated_as"selected text another selected text"`);
+  });
+
+  test('Test InstructionsArea component default mode with multiple selections)', () => {
+    props.mode = 'default';
+    props.selectionsReducer.selections = [{text:'selected text'}, {text:'another selected text'}, {text:'Alot of selections'}];
+    const wrapper = mount(<InstructionsArea {...props} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper.text()).toEqual(`"${props.alignedGLText}"translated_as"selected text … Alot of selections"`);
+  });
+
+
   test('Test InstructionsArea component no selections)', () => {
     props.selectionsReducer.selections = [];
     props.dontShowTranslation = true;
